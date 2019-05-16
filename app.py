db267616f60faecc9f6471893443b3d84d98ea9e
Gerald Joshua <geraldjoshua7@gmail.com>
2019-05-16 11:40:14 +0200

    working on the login page

--- app.py
+++ app.py
@@ -2,16 +2,26 @@
 #a very simple webapp implemented using flask
 
 
-from flask import Flask, render_template   #loading the Flask framework 
+from flask import Flask, redirect, render_template, request,url_for    #loading the Flask framework 
 
 app = Flask(__name__)      # creates a flask application to run my code
 
-@app.route('/')        #defines what happens when a person wants to go to locate "/" of the site
+#handle view and send data
+@app.route('/',methods=["GET","POST"])        #defines what happens when a person wants to go to locate "/" of the site
+
 
 
 def index():
-    return render_template("main_page.html")
+	if request.method =="GET":
+		return render_template("main_page.html", comments=comments)
     
-
+	comments.append(request.form["contents"])
+	return redirect(url_for('index'))
+	
+@app.route("/login/")
+def login():
+	return render_template("login_page.html")
+    
+comments= []
 if __name__ == '__main__':
     app.run(debug=True)
